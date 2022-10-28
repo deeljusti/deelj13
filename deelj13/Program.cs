da8872c6715bba6bb505234fc7aedb51fa4ac3d7
harold justi <harold.justi@etec.sp.gov,br>
2022-10-27 21:55:45 -0300

    projeto inicial

---
+++ deelj13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deelj13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numero2;
+            int total;
+            numero = 10;
+            numero2 = 3;
+              
+            total = numero + numero2;
+         
+            Console.WriteLine("boa noite companheiros e companheiras");
+            Console.WriteLine("domingo é 13 ");
+            Console.WriteLine(" lula ");
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
